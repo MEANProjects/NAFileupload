057024a8b1687909502d0f1bd5d80766b856ef0c
chandrasekar kannaiah <chandrasekar.kannaiah@ixonos.com>
2017-01-27 17:57:20 +0200

    new frisby casde for sending form data
    Signed-off-by: chandrasekar kannaiah <chandrasekar.kannaiah@ixonos.com>

--- frisby/fileupload_spec.js
+++ frisby/fileupload_spec.js
@@ -2,6 +2,8 @@ var frisby = require('frisby');
 var fs = require('fs');
 var path = require('path')
 
+var FormData = require('form-data');
+
 var filePath = path.resolve(__dirname, '../testfile.txt');
 var fileSize = fs.statSync(filePath).size;
 var fileContent = fs.readFileSync(filePath);
@@ -13,18 +15,35 @@ frisby.create('File upload test')
     {rawContent: fileContent})
   .expectStatus(200)
   /*.expectHeaderContains('content-type', 'application/json')
-  .expectJSON({
-    data: 'data:application/octet-stream;base64,' + fileContent.toString('base64'),
-    headers: {
-      "Content-Type": "application/octet-stream",
-      "Content-Length": String(fileSize)
-    },
-    url: "http://localhost:8000/uploadfile",
-    json: null,
-    files: {},
-    form: {}
-  })
-  .expectJSONTypes({
-    data: String
-  })*/
+   .expectJSON({
+   data: 'data:application/octet-stream;base64,' + fileContent.toString('base64'),
+   headers: {
+   "Content-Type": "application/octet-stream",
+   "Content-Length": String(fileSize)
+   },
+   url: "http://localhost:8000/uploadfile",
+   json: null,
+   files: {},
+   form: {}
+   })
+   .expectJSONTypes({
+   data: String
+   })*/
+  .toss();
+
+var form = new FormData();
+form.append('rawContent', fs.createReadStream(filePath), {
+  knownLength: fs.statSync(filePath).size
+});
+
+frisby.create('File upload test using form data')
+  .post('http://localhost:8000/uploadfile',
+    form, {
+      json: false,
+      headers: {
+        'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
+        'content-length': form.getLengthSync(),
+      }
+    })
+  .expectStatus(200)
   .toss();
